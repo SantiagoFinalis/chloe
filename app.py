5c8984f5a80dd098ccef86497b534836e8d67fb7
RodriFinalis <159319714+RodriFinalis@users.noreply.github.com>
2024-04-29 16:59:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import requests
 
 app = Flask(__name__)
 
-@app.route('/validate', methods=['POST'])
+app.route('/validate', methods=['POST'])
 def validate_input():
     app.logger.debug("Received validation request")
 
@@ -45,14 +45,15 @@ def validate_input():
             'first_name': data.get('first_name'),
             'last_name': data.get('last_name'),
             'email': data.get('email'),
-            'phone': data.get('phone_number'),
+            'phoneNumber': data.get('phone_number'),
             'linkedin_url': data.get('linkedin_url'),
-            'country': data.get('country_of_residence'),
-            'state': data.get('state'),
-            'finra_crd': crd_number,
-            'business_description': data.get('business_description'),
-            'referral_source': data.get('how_did_you_hear_about_us'),
-            'other_info': data.get('other')
+            'typeA': data.get('country_of_residence'),
+            'typeA14': data.get('state'),
+            'do_you_have_a_finra_crd': 'yes',  # Assume yes since a valid CRD number exists
+            'crd': crd_number,
+            'briefDescription': data.get('business_description'),
+            'howDid': data.get('how_did_you_hear_about_us'),
+            'other': data.get('other')
         }
 
         # Send a POST request to Pardot only if CRD is valid
